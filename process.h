1d0ab248b7471d4bca39b58175004c398af3a144
ggonee98 <90853573+ggonee98@users.noreply.github.com>
2022-03-22 15:16:53 +0900

    Delete process.h

--- process.h
+++
@@ -1,8 +0,0 @@
-int count_student(FILE* fp);
-
-void process_create();
-void process_load(char tok2);
-void process_print();
-void process_insert(char tok2, char tok3, char tok4, char tok5, char tok6, char tok7, char tok8);
-void process_delete(char tok2);
-void process_search(char tok2);
